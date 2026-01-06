475a2a987bfcfeefbc5ca7d63ef3d790c7cc348d
Yuuta Suzuki <231205106@ccmailg.meijo-u.ac.jp>
2026-01-06 14:53:19 +0900

    update
    動物のイベント

---
+++ Assets/Scenes/Script/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private float timeLimit;
+
+    private TimerManager timerManager;
+
+    void Update()
+    {
+        timerManager = GetComponent<TimerManager>();
+
+        if (timerManager != null)
+        {
+            timeLimit = timerManager.timeLimit;
+        }
+    }
+}
+
+
